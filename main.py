380fd36eafafb660cc387fb9cf96e98e7c9dc0fa
Rustom Ichhaporia <45826892+rustom@users.noreply.github.com>
2022-11-30 19:03:27 -0600

    Add environment example

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
+auth_manager = SpotifyClientCredentials()
+sp = spotipy.Spotify(auth_manager=auth_manager)
+
+# playlists = sp.user_playlists('shidoarichimorin')
+# while playlists:
+#     for i, playlist in enumerate(playlists['items']):
+#         print("%4d %s %s" % (i + 1 + playlists['offset'], playlist['uri'],  playlist['name']))
+#     if playlists['next']:
+#         playlists = sp.next(playlists)
+#     else:
+#         playlists = None
+        
+saved
